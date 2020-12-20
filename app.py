5715772c6354851e0da80754350f4b3673951492
Nguyễn Khắc Minh (VNCDLL-CTPTNLVKHDL) <minhnk@vingroup.net>
2020-12-20 16:17:28 +0700

    demo

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from __future__ import print_function
 from flask import Flask, render_template,request,json,redirect
 from infer import ModelInfer
+import numpy as np
 app = Flask(__name__)
 model = ModelInfer()
 @app.route('/',methods=['GET','POST'] )
 def welcome():
+    return render_template('test2.html')
 
 
-    return render_template('Demo.html')
 
+definitions = {
+    "A": "Hihihi",
+    "AC": "Haha",
+    "TV": "Nung cho ha m"
+}
 
 @app.route('/gen',methods=['POST'])
 def vote():
@@ -19,20 +25,28 @@ def vote():
     print(st2)
     text=""
 
-    p='<p align="center" class="tag_{}">{} <br> {}</p>'
+    p='<mark data-entity="{}" d-entity="{}" class="radius_option" data-toggle="popover" title="{}" data-trigger="hover" data-content="{}">{}</mark>'
 
     for i in range(len(st2)):
         label=st2[i]
         word=st1[i]
         if label=="O":
-            text += p.format(label, word, "")
+            text += ' ' + word
         else:
-            text+=p.format(label,word,label)
+            text+=p.format(label,label,label, definitions.get(label) if label in definitions.keys() else "kimochi",word)
 
+    
 
     text={'text':text}
-    # print(text)
-    return json.dumps({'success': True, 'text_tagged': text}), 200, {'Content-Type': 'application/json; charset=UTF-8'}
+
+    if 'O' in st2:
+        st2.remove('O')
+    unique, counts = np.unique(st2, return_counts=True)
+    c = [str(x) for x in counts] 
+    t = dict(zip(unique, c))
+    print(text)
+    print(t)
+    return json.dumps({'success': True, 'text_tagged': text, 'tags':t}), 200, {'Content-Type': 'application/json; charset=UTF-8'}
 
 
 
@@ -40,7 +54,7 @@ def vote():
 if __name__=='__main__':
     test_sentence = "xe máy đi với tốc độ hơn 80 km/h bị phạt bao nhiều tiền"
     # test_sentence
-    app.run(host='0.0.0.0',debug=False,port=5000)
+    app.run(host='localhost',debug=False,port=8080)
 
 
 
